e7684fa73d48aecb9fc69555cc182618f2eb5773
Bommineni Srinivasulu <srinivasulu.bommineni24@gmail.com>
2025-07-05 23:55:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 // Mobile menu toggle
-window.location.href = "birthday.html";
+if (!localStorage.getItem('hasRedirected')) {
+    localStorage.setItem('hasRedirected', 'true');
+    window.location.href = "https://mr-srinu.github.io/chhaatra/bday.html"; // replace with your page
+  }
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('header nav');
 menuBtn.addEventListener('click', () => {
