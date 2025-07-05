f314a8e41e6472e82dbcd106e9c961a1564426b8
Bommineni Srinivasulu <srinivasulu.bommineni24@gmail.com>
2025-07-05 13:30:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Mobile menu toggle
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('header nav');
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
