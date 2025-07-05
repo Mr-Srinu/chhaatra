000c908b60d184661727f81bd87649613d1972f4
Bommineni Srinivasulu <srinivasulu.bommineni24@gmail.com>
2025-07-06 00:03:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // Mobile menu toggle
-if (!localStorage.getItem('hasRedirected')) {
-    localStorage.setItem('hasRedirected', 'true');
-    window.location.href = "https://mr-srinu.github.io/chhaatra/bday.html"; // replace with your page
-  }
+// if (!localStorage.getItem('hasRedirected')) {
+//     localStorage.setItem('hasRedirected', 'true');
+//     window.location.href = "https://mr-srinu.github.io/chhaatra/bday.html"; // replace with your page
+//   }
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('header nav');
 menuBtn.addEventListener('click', () => {
